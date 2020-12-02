d317d132fe6e010b5433140de4999a7ecbeba72f
njean42 <gunoi@njean.me>
2020-12-02 19:44:49 +0100

    Split panels that are tied together by a speech bubble (before this, they would be detected as just one panel).

--- kumikolib.py
+++ kumikolib.py
@@ -4,6 +4,7 @@
 
 import os
 import cv2 as cv
+import numpy as np
 
 
 
@@ -50,6 +51,67 @@ class Kumiko:
 		return infos
 	
 	
+	def dots_are_close(self,dot1,dot2):
+		max_dist = self.gutterThreshold * 2
+		return abs(dot1[0]-dot2[0]) < max_dist and abs(dot1[1]-dot2[1]) < max_dist
+	
+	
+	def split_polygon(self,polygon):
+		
+		close_dots = []
+		for i in range(len(polygon)-1):
+			all_close = True
+			for j in range(i+1,len(polygon)):
+				dot1 = polygon[i][0]
+				dot2 = polygon[j][0]
+				
+				if self.dots_are_close(dot1,dot2):
+					if not all_close:	
+						close_dots.append([i,j])
+				else:
+					all_close = False
+		
+		if len(close_dots) == 0:
+			return [polygon]
+		
+		# take the dots that shouldn't be close, i.e. those with the most hops in between (other dots)
+		best_cut = max(close_dots, key=lambda i: abs(i[1]-i[0]) % (len(polygon)-1))
+		# NOTE: The first (i=0) and last dots are connected. There's at most len(polygon)-1 hops between two dots
+		
+		poly1len = len(polygon) - best_cut[1] + best_cut[0]
+		poly2len = best_cut[1] - best_cut[0]
+		poly1 = np.zeros(shape=(poly1len,1,2), dtype=int)
+		poly2 = np.zeros(shape=(poly2len,1,2), dtype=int)
+		
+		x = y = 0
+		for i in range(len(polygon)):
+			if i <= best_cut[0] or i > best_cut[1]:
+				poly1[x][0] = polygon[i]
+				x += 1
+			else:
+				poly2[y][0] = polygon[i]
+				y += 1
+		
+		return [poly1,poly2]  # TODO: recurse?
+	
+	
+	def merge_panels(self, panels):
+		panels_to_remove = []
+		for i in range(len(panels)):
+			for j in range(i+1,len(panels)):
+				if panels[i].contains(panels[j]):
+					panels_to_remove.append(j)
+				elif panels[j].contains(panels[i]):
+					panels_to_remove.append(i)
+		
+		for i in reversed(panels_to_remove):
+			del panels[i]
+	
+	
+	def getGutterThreshold(size):
+		return sum(size) / 2 / 20
+	
+	
 	def parse_image(self,filename):
 		img = self.read_image(filename)
 		# TODO: handle error
@@ -63,7 +125,7 @@ class Kumiko:
 			'panels': []
 		}
 		
-		self.gutterThreshold = sum(infos['size']) / 2 / 20
+		self.gutterThreshold = Kumiko.getGutterThreshold(infos['size'])
 		
 		gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 		
@@ -76,23 +138,28 @@ class Kumiko:
 		
 		# Get (square) panels out of contours
 		for contour in contours:
-			
 			arclength = cv.arcLength(contour,True)
-			
 			epsilon = 0.01 * arclength
 			approx = cv.approxPolyDP(contour,epsilon,True)
 			
-			x,y,w,h = cv.boundingRect(approx)
+			# See if this panel can be cut into several (two non-consecutive points are close)
+			polygons = self.split_polygon(approx)
 			
-			# exclude very small panels
-			if w < infos['size'][0]/15 or h < infos['size'][1]/15:
-				continue
-			
-			contourSize = int(sum(infos['size']) / 2 * 0.004)
-			cv.drawContours(img, [approx], 0, (0,0,255), contourSize)
-			
-			panel = Panel([x,y,w,h], self.gutterThreshold)
-			infos['panels'].append(panel)
+			for p in polygons:
+				x,y,w,h = cv.boundingRect(p)
+				
+				# exclude very small panels
+				if w < infos['size'][0]/15 or h < infos['size'][1]/15:
+					continue
+				
+				contourSize = int(sum(infos['size']) / 2 * 0.004)
+				cv.drawContours(img, [p], 0, (0,0,255), contourSize)
+				
+				panel = Panel([x,y,w,h], self.gutterThreshold)
+				infos['panels'].append(panel)
+		
+		# Merge every two panels where one contains the other
+		self.merge_panels(infos['panels'])
 		
 		if len(infos['panels']) == 0:
 			infos['panels'].append( Panel([0,0,infos['size'][0],infos['size'][1]], self.gutterThreshold) );
@@ -132,7 +199,7 @@ class Panel:
 		return [self.x, self.y, self.w, self.h]
 	
 	def __eq__(self, other):
-		return self.x == other.x and self.y == other.y and self.w == other.w and self.h == other.h  # TODO: include gutterThreshold
+		return abs(self.x-other.x) < self.gutterThreshold and abs(self.y == other.y) < self.gutterThreshold and abs(self.r == other.r) < self.gutterThreshold and abs(self.b == other.b) < self.gutterThreshold
 	
 	def __lt__(self, other):
 		# panel is above other
@@ -161,3 +228,27 @@ class Panel:
 	
 	def __ge__(self, other):
 		return self.__gt__(other)
+	
+	def area(self):
+		return self.w * self.h
+	
+	def __str__(self):
+		return "[[{0},{1}],[{2},{3}],[{4},{5}],[{6},{7}]]".format(self.x,self.y, self.r,self.y, self.r,self.b, self.x,self.b)
+	
+	# returns an overlapping percentage
+	def contains(self,other):
+		if self.x > other.r or other.x > self.r:  # panels are left and right from one another
+			return 0
+		if self.y > other.b or other.y > self.b:  # panels are above and below one another
+			return 0
+		
+		# if we're here, panels overlap at least a bit
+		x = max(self.x,other.x)
+		y = max(self.y,other.y)
+		r = min(self.r,other.r)
+		b = min(self.b,other.b)
+		
+		overlap_panel = Panel([x,y,r-x,b-y], self.gutterThreshold)
+		
+		# panels overlap if the overlapping area is more than 75% of the smallest one's area
+		return overlap_panel.area() / other.area() > 0.75
