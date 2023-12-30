a1b321fe5bac0d8c68b0ab6703b98bd364fcdda2
njean42 <gunoi@njean.me>
2023-12-30 14:42:08 +0100

    simplify tester + display processing time for each version

--- tester.py
+++ tester.py
@@ -6,6 +6,7 @@ import json
 import argparse
 import re
 import tempfile
+import time
 from lib.debug import Debug
 from lib.html import HTML
 
@@ -15,7 +16,6 @@ class Tester:
 	files = []
 	git_repo = '.'
 	git_versions = [
-		# 'v1.0', 'v1.1', 'v1.1.1', 'v1.2', 'v1.2.1', 'v1.3', 'v1.4', 'v1.4.1',
 		'v1.4.2',
 		'current',
 	]
@@ -41,43 +41,37 @@ class Tester:
 			self.files.sort(key = lambda d: d.name)
 
 	def run_all(self):
+		elapsed = {}
 		for git_version in self.git_versions:
-			self.run(git_version)
+			elapsed[git_version] = self.run(git_version)
+
+		print("\n\n########## Elapsed time for each version: ##########")
+		for version, time in elapsed.items():
+			print(f"{version}: {time:.0f} seconds")
 
 	def run(self, git_version):
 		print('\n\n########## Finding panels with kumiko version', git_version, '##########')
 		kumiko_bin = './kumiko'
 
-		# Kumiko would not accept non-image files before v1.2, special case for .licence files
-		accepts_license_files = git_version >= 'v1.2' or not re.match(r'^v\d+.\d+$', git_version)
-
-		with tempfile.TemporaryDirectory(dir = './tests') as tmpfolder:
+		with tempfile.TemporaryDirectory() as tempgit:
 			# Get that Kumiko version
 			if git_version != 'current':
-				tempgit = '/tmp/kumikogit'  # TODO use tempdir
-				subprocess.run(['rm', '-rf', tempgit], check = True)
 				subprocess.run(['git', 'clone', self.git_repo, tempgit], capture_output = True, check = True)
 				subprocess.run(['git', 'checkout', git_version], cwd = tempgit, capture_output = True, check = True)
 				kumiko_bin = os.path.join(tempgit, 'kumiko')
 
-			for f in self.files:
-				# move .license files to tempdir (<v1.2 compatibility)
-				if os.path.isdir(f) and not accepts_license_files:
-					for g in os.scandir(f):
-						if re.search(r'\.license$', g.name):
-							os.rename(g, os.path.join(tmpfolder, os.path.basename(g)))
+			t1 = time.time()
 
+			for f in self.files:
 				file_or_dir = f if isinstance(f, str) else f.name
 				print(f"\n##### Kumiko-cutting {file_or_dir} ({git_version}) #####")
 
 				subprocess.run(['mkdir', '-p', os.path.join(self.savedir, git_version)], check = True)
 				jsonfile = os.path.join(self.savedir, git_version, os.path.basename(f) + '.json')
+
 				subprocess.run(args = [kumiko_bin, '-i', f, '-o', jsonfile, '--progress'], check = True)
 
-				if os.path.isdir(f) and not accepts_license_files:
-					for g in os.scandir(tmpfolder):
-						if re.search(r'\.license$', g.name):
-							os.rename(g, os.path.join(f, os.path.basename(g)))
+			return time.time() - t1
 
 	def compare_all(self):
 		for i in range(len(self.git_versions) - 1):
